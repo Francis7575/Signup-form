29f9ff924a8c056f6a9a47629124d11daea18aca
Francis7575 <francis.albert.2009@gmail.com>
2024-03-29 16:18:02 -0700

    updated

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import styles from './Form.module.css';
 import { useState, FormEvent, ChangeEvent } from 'react';
-import SvgContent from '../../assets/icon-error.svg';
+import InputWithError from './InputWithError';
 
 type FormData = {
     firstname: string,
@@ -12,44 +12,6 @@ type FormData = {
 type Error = {
     message: string;
     fieldName: string;
-};
-
-const InputWithError: React.FC<{
-    value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    name: string;
-    placeholder: string;
-    error: Error | undefined;
-    type: string;
-    onFocus: (fieldName: string) => void;
-    fieldName: string;
-}> = ({ value, onChange, name, placeholder, error, type, onFocus, fieldName }) => {
-    const [isFocused, setIsFocused] = useState<boolean>(false);
-    const handleFocus = () => {
-        setIsFocused(true)
-        onFocus(fieldName);
-    }
-    const errorClassName = error ? styles.inputError : '';
-
-    return (
-        <div className={styles.inputContainer}>
-            <input
-                value={value}
-                onChange={onChange}
-                onFocus={handleFocus}
-                type={type}
-                name={name}
-                placeholder={placeholder}
-                className={`${styles.input} ${errorClassName} ${isFocused ? styles.focused : ''}`}
-            />
-            {error && error.fieldName === name && (
-                <div className={styles.errorContainer}>
-                    <p className={styles.errorMessage}>{error.message}</p>
-                    <img src={SvgContent} className={styles.errorIcon} alt="Error Icon" />
-                </div>
-            )}
-        </div>
-    );
 };
 
 const Form = () => {
---
+++ src/components/Form/InputWithError.tsx
@@ -0,0 +1,48 @@
+import { useState, ChangeEvent } from 'react';
+import styles from './InputWithError.module.css';
+import SvgContent from '../../assets/icon-error.svg';
+
+type Error = {
+    message: string;
+    fieldName: string;
+};
+
+const InputWithError: React.FC<{
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    name: string;
+    placeholder: string;
+    error: Error | undefined;
+    type: string;
+    onFocus: (fieldName: string) => void;
+    fieldName: string;
+}> = ({ value, onChange, name, placeholder, error, type, onFocus, fieldName }) => {
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const handleFocus = () => {
+        setIsFocused(true)
+        onFocus(fieldName);
+    }
+    const errorClassName = error ? styles.inputError : '';
+
+    return (
+        <div className={styles.inputContainer}>
+            <input
+                value={value}
+                onChange={onChange}
+                onFocus={handleFocus}
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                className={`${styles.input} ${errorClassName} ${isFocused ? styles.focused : ''}`}
+            />
+            {error && error.fieldName === name && (
+                <div className={styles.errorContainer}>
+                    <p className={styles.errorMessage}>{error.message}</p>
+                    <img src={SvgContent} className={styles.errorIcon} alt="Error Icon" />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default InputWithError;
